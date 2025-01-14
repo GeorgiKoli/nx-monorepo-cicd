5936b06d548c201741aa524c4356072a8ca08ccb
kolisg <georgi.kolishovski@jax.org>
2025-01-14 16:49:37 -0500

    removes the default nx-welcome component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 import { JaxAutocompleteComponent } from '@jax-compsci/ng-jax-components';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, JaxAutocompleteComponent],
+  imports: [RouterModule, JaxAutocompleteComponent],
   selector: 'ng-jax-components-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
